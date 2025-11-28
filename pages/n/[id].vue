dcd4ca0be66570a938961b89c412f42feb03c55c
Kerlon <kerlonbrito0@gmail.com>
2025-11-28 16:20:22 -0300

    -

--- pages/n/[id].vue
+++ pages/n/[id].vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="p-4 max-w-xl mx-auto">
-    <p v-if="!anotacao" class="text-gray-500">Carregando...</p>
-    <pre v-else class="whitespace-pre-wrap bg-gray-100 p-4 rounded">
-{{ anotacao }}
-    </pre>
-    </div>
+<p v-if="pending" class="text-gray-500">Carregando...</p>
+
+<pre v-else class="whitespace-pre-wrap bg-gray-100 p-4 rounded">
+{{ anotacao.texto }}
+</pre>
+
+<p v-if="error">Erro: {{ error.message }}</p>
 </template>
 
 <script setup>
-const route = useRoute();
+const route = useRoute()
 
-const { texto } = await $fetch(`/api/anotacao/${route.params.id}`);
-const anotacao = ref(texto);
+const { data: anotacao, pending, error } = await useAsyncData(
+  () => $fetch(`/api/anotacao/${route.params.id}`)
+)
 </script>

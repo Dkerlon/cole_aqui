4b078d7969b21f1bc7893186164a63de96a8a591
Kerlon <kerlonbrito0@gmail.com>
2025-11-28 16:01:42 -0300

    -

--- server/utils/mongoose.ts
+++ server/utils/mongoose.ts
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 // import { useRuntimeConfig } from 'nuxt/app';
 
-
-const config = process.env;
-
 export async function connectDB() {
     if (mongoose.connection.readyState === 1) return;
+    console.log({'process.env.MONGODB_URI': process.env.MONGODB_URI})
 
-    await mongoose.connect(config.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
 }
